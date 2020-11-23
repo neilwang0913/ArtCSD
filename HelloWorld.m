e22f0bdaebad39c30ce19964ad099ac54d7baec5
Fz Wang <wangfengzhou002@gmail.com>
2020-11-23 13:33:31 +0000

    Update HelloWorld.m

--- HelloWorld.m
+++ HelloWorld.m
@@ -1,8 +1,8 @@
-%
+% Removes all variables from the current workspace, releasing them from system memory
 clear;
-%
+% Display version information for all installed products
 ver;
-%
+% Clear Command Window
 clc;
-%
-type hello_world
+% Display contents
+disp('你好,AI')
