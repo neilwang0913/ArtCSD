18c3869159aa60384db21afb0d9f0a421bd3b682
neilwang0913 <wangfengzhou002@gmail.com>
2022-10-03 21:38:15 +0100

    Co-authored-by:peterzhang17 <peterzhangla2017@gmail.com>

--- HelloWorld.m
+++ HelloWorld.m
@@ -1,11 +1,15 @@
 % Removes all variables from the current workspace, releasing them from system memory
 clear;
+
 % Display version information for all installed products
 ver;
+
 % Clear Command Window
 clc;
+
 % Display contents
 disp('Hello,AI')
+
 % 比较计算性能好坏，时间是最好的标准
 % comparing with diff time comsuption
 tic
